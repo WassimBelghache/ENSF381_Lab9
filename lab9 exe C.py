2e594004ab593ef1a637ff1200e5263567ae7ff7
cesarucal <cesar.garciar1@ucalgary.ca>
2024-03-20 15:33:30 -0600

    added header

---
+++ lab9 exe C.py
@@ -0,0 +1,9 @@
+'''
+= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+Name : images . html
+Assignment : Lab 9 Exercise C
+Author ( s ) : César García , Wassim Belghache
+Submission : March 20 , 2024
+Description : A simple HTML file .
+= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+'''
