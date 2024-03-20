143aa6eaf547862822055040043a40301b389f50
cesarucal <cesar.garciar1@ucalgary.ca>
2024-03-20 15:34:07 -0600

    changes

--- lab9 exe C.py
+++ lab9 exe C.py
@@ -1,9 +1,9 @@
 '''
 = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-Name : images . html
+Name : lab9_exe_C
 Assignment : Lab 9 Exercise C
 Author ( s ) : César García , Wassim Belghache
 Submission : March 20 , 2024
-Description : A simple HTML file .
+Description : Interacting with JSON Data in Python
 = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 '''
